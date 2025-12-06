bd0620e42758d93c10a612789203bf164fdabe1d
Shreya-2006 <shreyamanded69@gamil.com>
2025-12-06 08:56:10 +0530

    palin

---
+++ palin.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) > 1:
+    string = sys.argv[1]
+else:
+    string = input("Enter the string: ")
+
+if string.lower() == string[::-1].lower():
+    print(f"'{string}' is a palindrome")
+else:
+    print(f"'{string}' is not a palindrome")
